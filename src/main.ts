51244d5c65b558a39eb4dc2a1dfdc949117a7bcd
Fernanda Ek <fernandaek@icloud.com>
2020-02-17 17:37:15 +0100

    Fourth commit

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,8 @@ export class Main {
     service: IServiceTodo;
     constructor(service: IServiceTodo) {
         this.service = service;
+        this.deleteHandler = this.deleteHandler.bind(this);
+        this.strikeThrough = this.strikeThrough.bind(this);
     }
 
     items: Todo[] = [];
@@ -38,11 +40,9 @@ export class Main {
 
         for (let i = 0; i < list.length; i++) {
             document.getElementById(`close${i}`).addEventListener("click", () => {
+                event.stopPropagation();
                 this.items.splice(i, 1);
                 this.printTodos();
-                this.strikeThrough();
-                this.deleteButton();
-                this.deleteHandler();
             })
         }
     }
@@ -75,34 +75,20 @@ export class Main {
         else {
             document.getElementById("todoList").appendChild(li);
             this.printTodos();
-            this.strikeThrough();
-            this.deleteButton();
-            this.deleteHandler();
         }
         console.log(this.items)
     }
 
-    strikeThrough() {
-        let list = document.getElementsByTagName("LI");
-        for(let i = 0; i < list.length; i++){
-            document.getElementById(`list${i}`).addEventListener("click", () => {
-                let x = event.target as HTMLElement;
-                 if (this.items[i].IsCompleted == true) {
-                    this.items[i].IsCompleted = false
-                    x.classList.toggle("checked")
-                    console.log(this.items)
-                 }
-                 else if(this.items[i].IsCompleted == false){
-                    this.items[i].IsCompleted = true
-                    x.classList.toggle("checked")
-                    console.log(this.items)
-                 }
-                }
-
-            )
-
-         }
-        
+    strikeThrough(e) {
+        let x = e.target.id 
+        if(this.items[x].IsCompleted){
+            this.items[x].IsCompleted = false
+        }
+        else{
+            this.items[x].IsCompleted = true
+        }
+        console.log(this.items)
+        this.printTodos();   
     }
 
 
@@ -112,12 +98,14 @@ export class Main {
         for (let i in this.items) {
             if (this.items.hasOwnProperty(i) === true) {
                 let node = document.createElement("LI");
-                node.setAttribute("id", `list${i}`)
+                node.setAttribute("id", i)
 
                 let textNode = document.createTextNode(this.items[i].Text)
 
                 node.addEventListener('click', this.strikeThrough);
                 node.appendChild(textNode);
+
+                
 
                 ul.appendChild(node)
                 if (this.items[i].IsCompleted == true) {
@@ -126,6 +114,8 @@ export class Main {
                 }
             }
         }
+        this.deleteButton();
+        this.deleteHandler();
     }
 
 
@@ -134,15 +124,9 @@ export class Main {
         document.getElementById("sortBtn").addEventListener('click', () => {
             this.sortHandler()
             this.printTodos()
-            this.deleteButton()
-            this.deleteHandler()
-            this.strikeThrough()
         })
         document.getElementById("addBtn").addEventListener('click', () => this.createTodos());
         this.printTodos();
         console.log(this.items)
-        this.strikeThrough();
-        this.deleteButton();
-        this.deleteHandler();
     }
 }
